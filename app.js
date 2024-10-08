92dc2304c5bc91f68a1127870398ec9ca01f307d
Krishna M <krishnamuskawad31@gmail.com>
2024-10-08 16:41:42 +0530

    StayFinder

--- app.js
+++ app.js
@@ -6,8 +6,12 @@ const path = require("path");
 const methodOverride = require("method-override");
 // @ts-ignore
 const ejsMate = require("ejs-mate");
-mongoose.connect("mongodb://127.0.0.1:27017/stayfinder");
- 
+const wrapAsync = require("./utils/wrapAsync.js");
+const ExpressError = require("./utils/ExpressError.js");
+const listingSchema = require("./schema.js");
+
+mongoose.connect("mongodb://127.0.0.1:27017/wanderlust");
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({extended : true}));
@@ -32,49 +36,68 @@ app.get("/testListing", async (req, res)=>{
     res.send("The sample was saved successfully..!");
 });
  
-app.get("/listings", async (req, res)=>{
+// const validateListing = (req, res, next)=>{
+//     let {error} = listingSchema.validate(req.body);
+//     if(error){
+//         let errMsg = error.details.map((el)=> el.message).join(",");
+//         throw new ExpressError(400, errMsg);
+//     }else{
+//         next();
+//     }
+// }
+
+app.get("/listings", wrapAsync(async (req, res)=>{
     let allListings = await Listings.find();
     res.render("listings/index.ejs", {allListings});
-});
+}));
 
-app.get("/listings/new", async(req,res)=>{
+app.get("/listings/new", wrapAsync(async(req,res)=>{
     res.render("listings/new.ejs");
-});
+}));
 
-app.get("/listings/:id", async (req, res)=>{
+app.get("/listings/:id", wrapAsync(async (req, res)=>{
     let {id} = req.params;
     let listing = await Listings.findById(id);
     res.render("listings/show.ejs", {listing});
-});
+}));
 
-app.post("/listings", async(req, res)=>{
+app.post("/listings", wrapAsync(async(req, res)=>{
     let newListing = new Listings(req.body.listing);
     await newListing.save();
     res.redirect("/listings");
-});
+}));
 
-app.get("/listings/:id/edit", async (req, res)=>{
+app.get("/listings/:id/edit", wrapAsync(async (req, res)=>{
     let {id} = req.params;
     let listing = await Listings.findById(id);
     res.render("listings/edit.ejs", {listing});
-});
+}));
 
 
-app.put("/listings/:id", async (req, res)=>{
+app.put("/listings/:id", wrapAsync(async (req, res)=>{
     let {id} = req.params;
     await Listings.findByIdAndUpdate(id, { ...req.body.listing });
     res.redirect("/listings");
-});
+}));
 
-app.delete("/listings/:id", async(req, res)=>{
+app.delete("/listings/:id", wrapAsync(async(req, res)=>{
     let {id} = req.params;
     await Listings.findByIdAndDelete(id);
     res.redirect("/listings");
-});
+}));
 
 app.listen(8080, ()=>{
     console.log("The app is listening..!");
 });
-app.use((req, res)=>{
-    res.render("listings/pageNotFound.ejs");
+// app.use((req, res)=>{
+//     res.render("listings/pageNotFound.ejs");
+// });
+app.all("*", (req, res, next)=>{
+    next(new ExpressError(404, "Page not found..!"));
+});
+
+app.use((err, req, res, next)=>{
+    let {status = 500, message = "Something went wrong..!"} = err;
+    res.status(status).render("error.ejs", {message});
+    // res.status(status).send(message);
 });
